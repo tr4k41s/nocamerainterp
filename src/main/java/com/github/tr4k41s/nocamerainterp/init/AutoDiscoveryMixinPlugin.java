36b85b31a67a042fe64a71602de3c5e84c6d7dae
GitHub Action <action@github.com>
2025-12-13 23:01:51 +0000

    Initialize template repository proper
    Set package name to com.github.tr4k41s.nocamerainterp
    Set project name to nocamerainterp
    Set modid to nocamerainterp

--- src/main/java/com/github/tr4k41s/nocamerainterp/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/tr4k41s/nocamerainterp/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.tr4k41s.nocamerainterp.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/tr4k41s/nocamerainterp/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/tr4k41s/nocamerainterp/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.tr4k41s.nocamerainterp.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
